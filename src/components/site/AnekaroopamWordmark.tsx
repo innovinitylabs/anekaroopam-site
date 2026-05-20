789e4d1d00a69c6a1ff1cc6897663e7b0757aed8
innovinitylabs <rajasekarcmr@icloud.com>
2026-05-20 14:48:11 +0530

    change spacing from 4 to 6 px

--- src/components/site/AnekaroopamWordmark.tsx
+++ src/components/site/AnekaroopamWordmark.tsx
@@ -138,7 +138,7 @@ export function AnekaroopamWordmark({ className }: AnekaroopamWordmarkProps) {
         className="leading-none will-change-transform"
         aria-hidden
         initial={false}
-        animate={{ x: reduceMotion ? 0 : showTamil ? 4 : 0 }}
+        animate={{ x: reduceMotion ? 0 : showTamil ? 6 : 0 }}
         transition={rotationTransition}
       >
         {REST}
